af24cada1a2d293368499c5a2043dac11a14d3a4
DerEns86 <v.ens86@yahoo.de>
2025-03-24 14:07:43 +0100

    style: update AdminView layout and main.css for improved responsiveness and aesthetics

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -48,15 +48,15 @@ const clearMessanges = () => {
 </script>
 
 <template>
-  <section>
-    <h1>AdminView works</h1>
+  <section class="mx-auto p-4 flex flex-col items-center">
+    <h1 class="text-4xl font-semibold pb-4">FamNotify</h1>
     <button
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
       @click="clearMessanges"
     >
       Clear Messanges
     </button>
-    <div class="flex">
+    <div class="flex justify-between w-full max-w-100 mb-4">
       <div v-for="image in images" :key="image.imgName">
         <img
           :src="`/images/${image.imgName}`"
@@ -67,12 +67,16 @@ const clearMessanges = () => {
         />
       </div>
     </div>
-    <div class="flex">
-      <div v-for="gif in gifs" :key="gif.imgName">
+    <div class="gifContainer flex justify-center w-full max-w-100 flex-wrap gap-2">
+      <div
+        v-for="gif in gifs"
+        :key="gif.imgName"
+        class="w-full md:w-1/3 flex-grow flex justify-center mb-3"
+      >
         <img
           :src="`/images/${gif.imgName}`"
           :alt="gif.imgName"
-          class="icons"
+          class="gifs max-w-full scale-100 hover:scale-105 transition-transform duration-200 ease-in-out"
           @click="sendImageUrl(gif)"
           :class="{ disable: !gif.active && imageSent }"
         />
@@ -84,12 +88,44 @@ const clearMessanges = () => {
 
 <style scoped>
 .icons {
+  height: 100%;
   max-width: 5rem;
   max-height: 5rem;
-  object-fit: cover;
+  object-fit: contain;
+}
+
+.icons:hover {
+  cursor: pointer;
+  transform: scale(1.1);
 }
 
 .disable {
   filter: grayscale(1);
+}
+
+.gifs {
+  border-radius: 1rem;
+}
+
+.gifContainer div:last-of-type {
+  max-width: 70%;
+}
+
+@media screen and (max-width: 767px) {
+  .gifContainer div:last-of-type {
+    max-width: 100%;
+  }
+}
+
+@media screen and (max-width: 430px) {
+  .gifContainer {
+    flex-wrap: nowrap;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .gifs {
+    max-width: 100%;
+  }
 }
 </style>
